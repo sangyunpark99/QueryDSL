72cb4078d5f18ea9f876d9cc25dfd66f4bc8f8f7
psangyun5108@gmail.com <psangyun5108@gmail.com>
2025-02-16 19:00:53 +0900

    init: Querydsl 설정 및 검증

--- src/test/java/com/example/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/example/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.querydsl;
 
+import static org.assertj.core.api.Assertions.*;
+
+import com.example.querydsl.entity.Hello;
+import com.example.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+  @PersistenceContext
+  private EntityManager em;
 
   @Test
   void contextLoads() {
+    Hello hello = new Hello();
+    em.persist(hello);
+
+    JPAQueryFactory query = new JPAQueryFactory(em);
+    QHello qHello = new QHello("h");
+
+    Hello result = query
+        .selectFrom(qHello)
+        .fetchOne();
+
+    assertThat(result).isEqualTo(hello);
+    assertThat(result.getId()).isEqualTo(hello.getId());
   }
 
 }
