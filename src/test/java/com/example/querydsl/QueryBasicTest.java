01ac0b7cb6f0ae5b3649967b7dcb70f47450795e
sangyunpark99 <psangyun5108@gmail.com>
2025-02-18 21:49:53 +0900

    검색 조건 쿼리

--- src/test/java/com/example/querydsl/QueryBasicTest.java
+++ src/test/java/com/example/querydsl/QueryBasicTest.java
@@ -1,8 +1,5 @@
 package com.example.querydsl;
 
-import static com.example.querydsl.entity.QMember.member;
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.example.querydsl.entity.Member;
 import com.example.querydsl.entity.Team;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -13,58 +10,87 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.example.querydsl.entity.QMember.member;
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Transactional
 public class QueryBasicTest {
 
-  @Autowired
-  EntityManager em;
+    @Autowired
+    EntityManager em;
 
-  JPAQueryFactory queryFactory; // field-level로 이동
+    JPAQueryFactory queryFactory; // field-level로 이동
 
-  @BeforeEach
-  public void before() {
-    queryFactory = new JPAQueryFactory(em);
-    Team teamA = new Team("teamA");
-    Team teamB = new Team("teamB");
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
 
-    em.persist(teamA);
-    em.persist(teamB);
+        em.persist(teamA);
+        em.persist(teamB);
 
-    Member member1 = new Member("member1", 10, teamA);
-    Member member2 = new Member("member2", 11, teamA);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 11, teamA);
 
-    Member member3 = new Member("member3", 12, teamB);
-    Member member4 = new Member("member4", 13, teamB);
+        Member member3 = new Member("member3", 12, teamB);
+        Member member4 = new Member("member4", 13, teamB);
 
-    em.persist(member1);
-    em.persist(member2);
-    em.persist(member3);
-    em.persist(member4);
-  }
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
 
-  @Test
-  public void jpql() {
+    @Test
+    public void jpql() {
 
-    Member findMember = em.createQuery("select m from Member m where m.username= :username",Member.class)
-        .setParameter("username", "member1")
-        .getSingleResult();
+        Member findMember = em.createQuery("select m from Member m where m.username= :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
 
-    assertThat(findMember.getUsername()).isEqualTo("member1");
-  }
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 
-  @Test
-  public void queryDsl() {
-    // QMember를 static import해서 아래와 같은 방법으로 사용하는 것을 권장합니다.
+    @Test
+    public void queryDsl() {
+        // QMember를 static import해서 아래와 같은 방법으로 사용하는 것을 권장합니다.
 
-    // alias는 테이블 조인시 이름이 중복되는 것을 방지해주기 위해 존재한다.  
-    Member findMember = queryFactory
-        .select(member)
-        .from(member)
-        .where(member.username.eq("member1"))
-        .fetchOne();
-    // QueryDsl은 sql Injection을 막아줍니다.
+        // alias는 테이블 조인시 이름이 중복되는 것을 방지해주기 위해 존재한다.
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+        // QueryDsl은 sql Injection을 막아줍니다.
 
-    assertThat(findMember.getUsername()).isEqualTo("member1");
-  }
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void search() {
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1").and(member.age.between(10, 20)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getAge()).isEqualTo(10);
+    }
+
+    @Test
+    public void searchAndParam() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1")
+                        , member.age.eq(10)
+                )
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getAge()).isEqualTo(10);
+    }
 }
